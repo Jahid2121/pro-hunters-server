42bf0ddff04596534a5019c3da6e6d4c64e82c26
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2023-11-05 18:24:19 +0600

    cached node_modules

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
-require('dotenv').config()
 
 // middlewares
 app.use(cors())
